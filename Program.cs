d8c0688d3317d6ec76bf0d1736a570b726d3c6af
ASHISH YADAV <84899464+ashish-yadava@users.noreply.github.com>
2023-12-12 00:15:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapping_Two_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 30;
+            int b = 20;
+
+            Console.WriteLine(" a = " + a);
+            Console.WriteLine(" b = " + b);
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("-------------------Swapped--------------------");
+            Console.WriteLine(" a = " + a);
+            Console.WriteLine(" b = " + b);
+            Console.ReadLine();
+
+        }
+    }
+}
